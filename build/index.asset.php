3906b49654daddc70e8a55b43a47857963720595
VasilyShat <vasily.shatalkin.job@gmail.com>
2026-01-10 22:43:29 +0100

    003. Using InnerBlocks to Create a Nested Block

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'dfd1b36ad2f1f7141c39');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => 'ff109a9bd2d3ac9c471b');
