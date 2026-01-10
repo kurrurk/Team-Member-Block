01c50a9d5d2c6231011c28976bb3ad73fa6625f5
VasilyShat <vasily.shatalkin.job@gmail.com>
2026-01-10 22:31:36 +0100

    001. Initializing a New Plugin for Our Block and Editing the Boilerplate's Metadata

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'f5e6b814e1de75768b5b381e22df3b7f');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'dfd1b36ad2f1f7141c39');
