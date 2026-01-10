15e40b3aed974b88cb28c0066b15c5f9552d4815
VasilyShat <vasily.shatalkin.job@gmail.com>
2026-01-11 00:12:43 +0100

    007. Adding an option for changing the number of columns

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'a65c27a1682e01b1ebe3');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'b72d33a497e558165b5a');
--- src/edit.js
+++ src/edit.js
@@ -1,10 +1,35 @@
-//import { __ } from '@wordpress/i18n';
-import { useBlockProps, InnerBlocks } from '@wordpress/block-editor';
+import { __ } from '@wordpress/i18n';
+import {
+	useBlockProps,
+	InnerBlocks,
+	InspectorControls,
+} from '@wordpress/block-editor';
+import { PanelBody, RangeControl } from '@wordpress/components';
 import './editor.scss';
 
-export default function Edit() {
+export default function Edit( { attributes, setAttributes } ) {
+	const { columns } = attributes;
+
+	const onChangeColumns = ( newColumns ) => {
+		setAttributes( { columns: newColumns } );
+	};
 	return (
-		<div { ...useBlockProps() }>
+		<div
+			{ ...useBlockProps( {
+				className: `has-${ columns }-columns`,
+			} ) }
+		>
+			<InspectorControls>
+				<PanelBody>
+					<RangeControl
+						label={ __( 'Columns', 'team-members' ) }
+						min={ 1 }
+						max={ 6 }
+						onChange={ onChangeColumns }
+						value={ columns }
+					/>
+				</PanelBody>
+			</InspectorControls>
 			<InnerBlocks
 				allowedBlocks={ [ 'create-block/team-member' ] }
 				template={ [
@@ -12,7 +37,6 @@ export default function Edit() {
 					[ 'create-block/team-member' ],
 					[ 'create-block/team-member' ],
 				] }
-				templateLock="insert"
 			/>
 		</div>
 	);
