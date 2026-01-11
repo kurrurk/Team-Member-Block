05447f38ddb10a5c7b5295c32794a1ddfe05c79d
VasilyShat <vasily.shatalkin.job@gmail.com>
2026-01-11 11:14:18 +0100

    010. Introducing the ImagePlaceholder Component

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'bae39bbc33fc2a9bfed5');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '43f81d33154c83468db7');
--- src/team-member/edit.js
+++ src/team-member/edit.js
@@ -1,5 +1,9 @@
 import { __ } from '@wordpress/i18n';
-import { useBlockProps, RichText } from '@wordpress/block-editor';
+import {
+	useBlockProps,
+	RichText,
+	MediaPlaceholder,
+} from '@wordpress/block-editor';
 
 export default function Edit( { attributes, setAttributes } ) {
 	const { name, bio } = attributes;
@@ -13,6 +17,14 @@ export default function Edit( { attributes, setAttributes } ) {
 
 	return (
 		<div { ...useBlockProps() }>
+			<MediaPlaceholder
+				icon="admin-users"
+				// onSelect={}
+				// onSelectURL={}
+				// onError={}
+				accept="image/"
+				allowedTypes={ [ 'image' ] }
+			/>
 			<RichText
 				placeholder={ __( 'Member Name', 'team-member' ) }
 				tagName="h4"
